75219a0f417d88a6fd8871715ca24fd0a658e017
raycdut <raycdut@hotmail.com>
2019-10-17 12:42:24 +0800

    update

--- scrapyScripts/fund_scrapy/fund_scrapy/settings.py
+++ scrapyScripts/fund_scrapy/fund_scrapy/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -39,34 +39,35 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'fund_scrapy.middlewares.FundScrapySpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    # 'fund_scrapy.middlewares.FundScrapySpiderMiddleware': 543,
+    'scrapy.spidermiddlewares.offsite.OffsiteMiddleware': None
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'fund_scrapy.middlewares.FundScrapyDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'fund_scrapy.pipelines.FundScrapyPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
---
+++ scrapyScripts/fund_scrapy/fund_scrapy/spiders/fund_list.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FundListSpider(scrapy.Spider):
+    name = 'fund_list'
+    allowed_domains = ['http://fund.eastmoney.com']
+    start_urls = ['http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=1&letter=&gsid=&text=&sort=zdf,desc&page=1,200&dt=1571234671718&atfc=&onlySale=0']
+
+    def __init__(self):
+        self.start_index = 1  # start page index
+        self.max_index = 40  # max page index
+
+    def parse(self, response):
+        print('current page : {0}'.format(response.url))
+        nextUrl = self.get_next_page(response.status)
+        if self.start_index < self.max_index:
+            yield scrapy.Request(url=nextUrl, callback=self.parse)
+
+    def get_next_page(self, status):
+        if status == 200:
+            self.start_index = self.start_index+1
+
+        nextpage = 'http://fund.eastmoney.com/Data/Fund_JJJZ_Data.aspx?t=1&lx=1&letter=&gsid=&text=&sort=zdf,desc&page={0},200&dt=1571234671718&atfc=&onlySale=0'.format(
+            self.start_index)
+        return nextpage
