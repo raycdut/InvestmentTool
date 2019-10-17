a0f2474982642fca94b02dce1e9e49b54ccba627
raycdut <raycdut@hotmail.com>
2019-10-17 09:48:55 +0800

    init scrapy project

---
+++ scrapyScripts/tiantianfund.py
@@ -0,0 +1,14 @@
+
+
+class FundScrapy():
+    def __init__(self):
+        print('a')
+        pass
+
+    def get_all_funds(self):
+        start_page = 1
+        pass
+
+
+if __name__ == "__main__":
+    fundScrapy = FundScrapy()
