aad6bfab1c9e6ad03d7629dfbf617e075f3d65f8
raycdut <raycdut@hotmail.com>
2019-10-19 21:22:09 +0800

    add function  to send message to rabbitmq

--- scrapyScripts/fund_scrapy/fund_scrapy/pipelines.py
+++ scrapyScripts/fund_scrapy/fund_scrapy/pipelines.py
@@ -5,6 +5,8 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 from Mongo_Client import Mongo_Client
+import pika
+import json
 
 
 class FundScrapyPipeline(object):
@@ -26,3 +28,27 @@ class FundMongoPipeline(object):
     def process_item(self, item, spider):
         self.fund.insert_one(dict(item))
         return item
+
+
+class FundRabblitMQPipeline(object):
+    def __init__(self):
+        pass
+
+    def process_item(self, item, spider):
+
+        credentials = pika.PlainCredentials('raycdut', 'abc@1234')
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='192.168.3.18', credentials=credentials))
+        # 创建频道
+        channel = connection.channel()
+        # 创建一个队列名叫hello
+        channel.queue_declare(queue='fundlist')
+        # exchange -- 它使我们能够确切地指定消息应该到哪个队列去。
+        # 向队列插入数值 routing_key是队列名 body是要插入的内容
+        channel.basic_publish(exchange='',
+                              routing_key='fundlist',
+                              body=json.dumps(item._values))
+
+        print("开始队列")
+        # 缓冲区已经flush而且消息已经确认发送到了RabbitMQ中，关闭链接
+        connection.close()
--- scrapyScripts/fund_scrapy/fund_scrapy/settings.py
+++ scrapyScripts/fund_scrapy/fund_scrapy/settings.py
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -66,8 +66,9 @@ SPIDER_MIDDLEWARES = {
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'fund_scrapy.pipelines.FundPostgrePipeline': 300,
-    'fund_scrapy.pipelines.FundMongoPipeline': 200
+    # 'fund_scrapy.pipelines.FundPostgrePipeline': 300,
+    # 'fund_scrapy.pipelines.FundMongoPipeline': 200,
+    "fund_scrapy.pipelines.FundRabblitMQPipeline": 400
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
